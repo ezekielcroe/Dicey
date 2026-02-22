f832a6b1bc045d453b3a2f25cc7e9f1327a8e9c2
Yeo Zhi Zheng <zhizhengy@hcahospicecare.org.sg>
2026-02-22 11:01:09 +0800

    Initial Commit

---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Zhi Zheng Yeo on 22/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
